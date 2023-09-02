d04fbb4102ac27ac00aadd3ca358c7c1ed17a6ad
Emirhan Dilber <edilberfb@gmail.com>
2023-09-02 19:33:55 +0300

    a

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+    {
+        int x =3;
+        int y=4;
+        y +=2;
+        Console.WriteLine(y);
+        y /=1;
+        Console.WriteLine(y);
+    }
+    }
+}
